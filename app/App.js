df6ddaeb3c2613d85c5757d49ce9d436470f5745
Omar Einea <eineao@gmail.com>
2018-08-01 18:36:18 +0300

    Initialize app with requirements

---
+++ app/App.js
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return 'Hamlaty';
+  }
+}
+
+ReactDOM.render(<App/>, document.body);
